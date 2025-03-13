42e33e27821e2475c46080b9348a3eef35f19552
deparr <dparrott@protonmail.com>
2025-03-13 15:01:59 -0600

    feat: more of a plugin now

---
+++ lua/static/config.lua
@@ -0,0 +1,36 @@
+local M = {}
+
+---@class static.Config
+M.defaults = {
+  out_dir = "build/",
+  pages_dir = "pages/",
+  template_dir = "templates/",
+  template_main_slot = "::slot::",
+  static_dir = "static/",
+  default_title = "Test-Site",
+  templates = {},
+  root_pages = {
+    "/index",
+    "/404",
+  }
+}
+
+M.options = nil
+
+function M.setup(opts)
+  M.options = vim.tbl_deep_extend("force", M.defaults, opts or {})
+end
+
+function M.extend(opts)
+  return opts and vim.tbl_deep_extend("force", M.options, opts) or M.options
+end
+
+setmetatable(M, {
+  __index = function(_, k)
+    if k == "options" then
+      return M.defaults
+    end
+  end
+})
+
+return M
--- plugin/static.lua
+++ plugin/static.lua
@@ -1,3 +1,52 @@
+local static = require("static")
+local config = require("static.config")
+
 vim.api.nvim_create_user_command("StaticBuild", function(args)
-  require("static").build()
+  static.build()
 end, { bar = true })
+
+-- todo put this into the 'server'
+vim.api.nvim_create_user_command("StaticWatch", function(args)
+  if vim.g.static_watching then
+    vim.notify("already watching!")
+    return
+  end
+  vim.g.static_watching = true
+  if config.options == nil then
+    config.setup()
+  end
+
+  local opts = config.extend()
+  -- register autocmds ??
+  -- do initial build
+  -- start server
+  -- need a way to cancel
+
+  local augroup = vim.api.nvim_create_augroup("StaticWatchGroup", {})
+  -- todo a little jank
+  vim.api.nvim_create_user_command("StaticWatchStop", function(args)
+    vim.api.nvim_del_augroup_by_id(augroup)
+    vim.g.static_watching = false
+    vim.api.nvim_del_user_command("StaticWatchStop")
+    -- send kill message
+  end, {})
+
+  local cwd = vim.fn.getcwd()
+  local watch_dirs = {
+    vim.fs.joinpath(cwd, opts.pages_dir, "*.dj"),
+    vim.fs.joinpath(cwd, opts.template_dir, "*.html"),
+    vim.fs.joinpath(cwd, opts.static_dir, "*"),
+  }
+  vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    pattern = watch_dirs,
+    callback = function(event)
+      -- this should send a message to the 'server'
+      -- should be relatively short
+      vim.notify(event.file .. " changed!")
+    end,
+    group = augroup,
+    desc = "StaticWatchBufWritePost",
+  })
+
+  static.watch()
+end, {})
