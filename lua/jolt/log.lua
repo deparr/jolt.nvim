3aa01aeb0c0e7ab76b7c275911d0ab8a6d447d0a
deparr <dparrott@protonmail.com>
2025-03-21 14:34:57 -0600

    feat(log): write directly to stdout in headless

--- lua/jolt/log.lua
+++ lua/jolt/log.lua
@@ -3,14 +3,15 @@ local config = require("jolt.config")
 local M = {}
 
 local default_prefix = "jolt: "
-
 function M.scoped(scope)
   local prefix = (scope and #scope > 0) and ("jolt(%s): "):format(scope) or default_prefix
   return config.headless and function(msg)
-    vim.print(prefix .. msg)
+    -- vim.print(prefix .. msg)
+    io.write(prefix, msg, "\n")
   end or function(msg, l)
     vim.notify(prefix .. msg, l or vim.log.levels.INFO)
   end
+
 end
 
 M.default = M.scoped()
